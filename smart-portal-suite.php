6dfd3aa0b06b5ffb40f18536e576c0bf3b57041d
st183570 <st183570@stud.uni-stuttgart.de>
2026-09-25 09:43:18 +0200

    Version v.0.2.0

--- smart-portal-suite.php
+++ smart-portal-suite.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Smart Portal Suite
  * Plugin URI:        https://effizientes-heim.de/
  * Description:       Multi-Step Formulare für Gebäude-Check und Projektanfragen mit Nextcloud-Anbindung.
- * Version:           0.1.0
+ * Version:           0.2.0
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Jonah Kleimann
